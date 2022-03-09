34c43a8176625e2bcf796a240a1d6e2e7e7b2c1e
Aniket <aniketdhokane2071@gmail.com>
2022-03-09 23:25:41 +0530

    fetch data with useState and useEffect Hooks(traditional Way)

--- src/components/SuperHeroes.page.jsx
+++ src/components/SuperHeroes.page.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
+
+import axios from 'axios'
 
 const SuperHeroesPage = () => {
+    const [isLoading, setIsLoading] = useState(true)
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/superheroes').then(res => {
+      setData(res.data)
+      setIsLoading(false)
+    })
+  }, [])
+
+  if (isLoading) {
+    return <h2>Loading...</h2>
+  }
   return (
-    <div>SuperHeroes page</div>
+    <>
+        <h2>Super Heroes Page</h2>
+      {data.map(hero => {
+        return <div>{hero.name}</div>
+      })}
+    </>
   )
 }
 
